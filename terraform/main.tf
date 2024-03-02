a74af5a0d1a925a24ff451628d50534edf6e4203
AlexTLDR <alex.badragan@protonmail.com>
2024-03-02 21:04:58 +0100

    terraform initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,33 @@
+variable "certificate" {
+  type = map(string)
+  default = {}
+}
+
+provider "google" {
+    region = "europe-west10"
+    zone = "europe-west10-a"
+    project = "terraform-learn-416018"
+    credentials = var.certificate["key"]
+}
+
+resource "google_compute_instance" "default" {
+    name = "terraform-instance"
+    machine_type = "e2-micro"
+    tags = ["ssh"]
+
+    boot_disk{
+        initialize_params {
+            image = "debian-cloud/debian-10"
+        }
+    }
+
+    # install nginx
+    metadata_startup_script = "sudo apt-get update && sudo apt-get install -y nginx && sudo service nginx start"
+
+    network_interface {
+        network = "default"
+        access_config {
+            # nat_ip = ""
+        }
+    }
+}
