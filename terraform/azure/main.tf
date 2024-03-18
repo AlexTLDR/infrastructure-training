b2838a25203a34d0a774d15af29db93a155865c0
AlexTLDR <alex.badragan@protonmail.com>
2024-03-18 15:58:32 +0100

    azure virtual machine stub

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,76 +1,154 @@
-# Terraform Configuration -> to be moved in a separate provider.tf file
+/*
+
+Link to the documentation for the new blocks used
+in this terraform configuration file
+
+1.azurerm_windows_virtual_machine - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/windows_virtual_machine
+
+*/
 
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      source = "hashicorp/azurerm"
     }
   }
-
-  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
-  features {}
+  subscription_id = ""
+  tenant_id = ""
+  client_id = ""
+  client_secret = ""
+  features {}  
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
+locals {
+  resource_group_name="app-grp"
+  location="North Europe"
+  virtual_network={
+    name="app-network"
+    address_space="10.0.0.0/16"
+  }
+
+  subnets=[
+    {
+      name="subnetA"
+      address_prefix="10.0.0.0/24"
+    },
+    {
+      name="subnetB"
+      address_prefix="10.0.1.0/24"
+    }
+  ]
 }
 
-
-# Virtual Network and Subnet
-
-resource "azurerm_virtual_network" "myvnet" { 
-  name = "my-vnet" 
-  address_space = [ "10.0.0.0/16" ] 
-  location = "northeurope" 
-  resource_group_name = azurerm_resource_group.rg.name 
-} 
-
-resource "azurerm_subnet" "mysubnet" { 
-  name = "mySubnet" 
-  resource_group_name = azurerm_resource_group.rg.name 
-  virtual_network_name = azurerm_virtual_network.myvnet.name 
-  address_prefixes = "10.0.1.0/24" 
+resource "azurerm_resource_group" "appgrp" {
+  name     = local.resource_group_name
+  location = local.location  
 }
 
-# Public IP
+resource "azurerm_virtual_network" "appnetwork" {
+  name                = local.virtual_network.name
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  address_space       = [local.virtual_network.address_space]  
+  
+   depends_on = [
+     azurerm_resource_group.appgrp
+   ]
+  }
 
-resource "azurerm_public_ip" "myvm1publicip" { 
-  name = "pip1" 
-  location = "northeurope" 
-  resource_group_name = azurerm_resource_group.rg.name 
-  allocation_method = "Dynamic" 
-  sku = "Basic" 
+
+  resource "azurerm_subnet" "subnetA" {
+  name                 = local.subnets[0].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = [local.subnets[0].address_prefix]
+  depends_on = [
+    azurerm_virtual_network.appnetwork
+  ]
 }
 
-# Network Interface
-
-resource "azurerm_network_interface" "myvm1nic" { 
-  name = "myvm1-nic" 
-  location = "northeurope" 
-  resource_group_name = azurerm_resource_group.rg.name 
-
-  ip_configuration { 
-    name = "ipconfig1" 
-    subnet_id = azurerm_subnet.frontendsubnet.id 
-    private_ip_address_allocation = "Dynamic" 
-    public_ip_address_id = azurerm_public_ip.myvm1publicip.id 
-  } 
+resource "azurerm_subnet" "subnetB" {
+  name                 = local.subnets[1].name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = local.virtual_network.name
+  address_prefixes     = [local.subnets[1].address_prefix]
+  depends_on = [
+    azurerm_virtual_network.appnetwork
+  ]
 }
 
-resource "azurerm_windows_virtual_machine" "example" {
+resource "azurerm_network_interface" "appinterface" {
+  name                = "appinterface"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnetA.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.appip.id
+  }
+  depends_on = [
+    azurerm_subnet.subnetA
+  ]
+}
+
+resource "azurerm_public_ip" "appip" {
+  name                = "app-ip"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  allocation_method   = "Static"
+ depends_on = [
+   azurerm_resource_group.appgrp
+ ]
+}
+
+resource "azurerm_network_security_group" "appnsg" {
+  name                = "app-nsg"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 300
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  depends_on = [
+    azurerm_resource_group.appgrp
+  ]
+}
+
+resource "azurerm_subnet_network_security_group_association" "appnsglink" {
+  subnet_id                 = azurerm_subnet.subnetA.id
+  network_security_group_id = azurerm_network_security_group.appnsg.id
+}
+
+resource "azurerm_windows_virtual_machine" "appvm" {
   count                 = 9
   name                  = "myvm${count.index + 1}"
-  location              = "northeurope"
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.myvm1nic.id]
-  size                  = "Standard_B1s"
-  admin_username        = "adminuser"
-  admin_password        = "Password123!"
+  resource_group_name = local.resource_group_name
+  location            = local.location
+  size                = "Standard_D2S_v3"
+  admin_username      = "adminuser"
+  admin_password      = "Azure@123"
+  network_interface_ids = [
+    azurerm_network_interface.appinterface.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
@@ -78,9 +156,8 @@ resource "azurerm_windows_virtual_machine" "example" {
     sku       = "2019-Datacenter"
     version   = "latest"
   }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+  depends_on = [
+    azurerm_network_interface.appinterface,
+    azurerm_resource_group.appgrp
+  ]
 }
