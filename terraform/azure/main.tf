53d6b0e59eb1d4f9aeaad3cfe0f6802a727e21a7
AlexTLDR <alex.badragan@protonmail.com>
2024-03-21 10:46:04 +0100

    encountered Missing resource instance key error

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,154 +1,74 @@
-/*
-
-Link to the documentation for the new blocks used
-in this terraform configuration file
-
-1.azurerm_windows_virtual_machine - https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/windows_virtual_machine
-
-*/
-
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-    }
-  }
-}
-
 provider "azurerm" {
-  subscription_id = ""
-  tenant_id = ""
-  client_id = ""
-  client_secret = ""
-  features {}  
+  features {}
 }
 
-locals {
-  resource_group_name="app-grp"
-  location="North Europe"
-  virtual_network={
-    name="app-network"
-    address_space="10.0.0.0/16"
+# TODO: Move the variables in a separate file
+variable "vm_count" {
+  description = "Number of VMs to create"
+  default     = 2
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-alex-tf"
+  location = "West Europe"
+
+  tags = {
+    "Environment Type"  = "Test"
+    "Owner or Customer" = "Alex, DevOps"
+    "Review Date"       = "March 2024"
   }
-
-  subnets=[
-    {
-      name="subnetA"
-      address_prefix="10.0.0.0/24"
-    },
-    {
-      name="subnetB"
-      address_prefix="10.0.1.0/24"
-    }
-  ]
 }
 
-resource "azurerm_resource_group" "appgrp" {
-  name     = local.resource_group_name
-  location = local.location  
+# Virtual Network and Subnet
+
+resource "azurerm_virtual_network" "devops_vnet" { 
+  name = "devops-vnet" 
+  address_space = [ "10.0.0.0/16" ] 
+  location = "westeurope" 
+  resource_group_name = azurerm_resource_group.rg.name 
+} 
+
+resource "azurerm_subnet" "devops_subnet" { 
+  name = "devops_subnet" 
+  resource_group_name = azurerm_resource_group.rg.name 
+  virtual_network_name = azurerm_virtual_network.devops_vnet.name 
+  address_prefixes = ["10.0.1.0/24"] 
 }
 
-resource "azurerm_virtual_network" "appnetwork" {
-  name                = local.virtual_network.name
-  location            = local.location
-  resource_group_name = local.resource_group_name
-  address_space       = [local.virtual_network.address_space]  
-  
-   depends_on = [
-     azurerm_resource_group.appgrp
-   ]
+# Public IP
+
+resource "azurerm_public_ip" "devops_publicip" { 
+  name = "devops-publicip" 
+  location = "westeurope" 
+  resource_group_name = azurerm_resource_group.rg.name 
+  allocation_method = "Dynamic" 
+  sku = "Basic" 
+}
+
+# Network Interface
+
+resource "azurerm_network_interface" "devops_nic" { 
+  count = var.vm_count
+  name = "devops-nic-${count.index}" 
+  location = "westeurope" 
+  resource_group_name = azurerm_resource_group.rg.name 
+
+  ip_configuration { 
+    name = "ipconfig1" 
+    subnet_id = azurerm_subnet.devops_subnet.id 
+    private_ip_address_allocation = "Dynamic" 
   }
-
-
-  resource "azurerm_subnet" "subnetA" {
-  name                 = local.subnets[0].name
-  resource_group_name  = local.resource_group_name
-  virtual_network_name = local.virtual_network.name
-  address_prefixes     = [local.subnets[0].address_prefix]
-  depends_on = [
-    azurerm_virtual_network.appnetwork
-  ]
 }
 
-resource "azurerm_subnet" "subnetB" {
-  name                 = local.subnets[1].name
-  resource_group_name  = local.resource_group_name
-  virtual_network_name = local.virtual_network.name
-  address_prefixes     = [local.subnets[1].address_prefix]
-  depends_on = [
-    azurerm_virtual_network.appnetwork
-  ]
-}
-
-resource "azurerm_network_interface" "appinterface" {
-  name                = "appinterface"
-  location            = local.location
-  resource_group_name = local.resource_group_name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnetA.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.appip.id
-  }
-  depends_on = [
-    azurerm_subnet.subnetA
-  ]
-}
-
-resource "azurerm_public_ip" "appip" {
-  name                = "app-ip"
-  resource_group_name = local.resource_group_name
-  location            = local.location
-  allocation_method   = "Static"
- depends_on = [
-   azurerm_resource_group.appgrp
- ]
-}
-
-resource "azurerm_network_security_group" "appnsg" {
-  name                = "app-nsg"
-  location            = local.location
-  resource_group_name = local.resource_group_name
-
-  security_rule {
-    name                       = "AllowRDP"
-    priority                   = 300
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "3389"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
-  depends_on = [
-    azurerm_resource_group.appgrp
-  ]
-}
-
-resource "azurerm_subnet_network_security_group_association" "appnsglink" {
-  subnet_id                 = azurerm_subnet.subnetA.id
-  network_security_group_id = azurerm_network_security_group.appnsg.id
-}
-
-resource "azurerm_windows_virtual_machine" "appvm" {
-  count                 = 9
+resource "azurerm_windows_virtual_machine" "devops_poc_vm" {
+  count                 = var.vm_count
   name                  = "myvm${count.index + 1}"
-  resource_group_name = local.resource_group_name
-  location            = local.location
-  size                = "Standard_D2S_v3"
-  admin_username      = "adminuser"
-  admin_password      = "Azure@123"
-  network_interface_ids = [
-    azurerm_network_interface.appinterface.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+  location              = "westeurope"
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.devops_nic.id]
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  admin_password        = "Password123!"
 
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
@@ -156,8 +76,9 @@ resource "azurerm_windows_virtual_machine" "appvm" {
     sku       = "2019-Datacenter"
     version   = "latest"
   }
-  depends_on = [
-    azurerm_network_interface.appinterface,
-    azurerm_resource_group.appgrp
-  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 }
